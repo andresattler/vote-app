84dc7f3e330970eacfdd84300f1d9753665bccba
Andre Sattler <andresattler.webdev@gmail.com>
2018-05-01 14:09:03 +0200

    changed from tsc to babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,22 +7,19 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '/dist')
   },
-
-  // Enable sourcemaps for debugging webpack's output.
   devtool: 'source-map',
-
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: ['.ts', '.tsx', '.js', '.json']
   },
-
   module: {
     rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+      { test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
       { test: /\.scss/, loader: 'style-loader!css-loader!sass-loader' },
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
     ]
   },
   plugins: [
